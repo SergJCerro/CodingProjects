64669a49e9d57a4565e07271a6e566295d761a8c
SergJCerro <stuasvsstuas@gmail.com>
2021-07-27 18:14:17 -0600

    NEET

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function log() {
+    setTimeout(
+      console.log.bind(
+        console,
+        "%c Created by : Sergio J Cerritos %c",
+        "background-color: #bdd4e7;background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);padding:5px;border-radius: 5px;line-height: 26px;",
+        ""
+      )
+    );
+    
+  }
+  log();
+
+
+
+
+  function getinTouch(){
+      var business = "UnBelizeable Web Designer"
+      var url = "https://unbelizeablewd.com"
+      var name = "Sergio Cerritos";
+      var title = "Owner";
+      var phone = "+501-602-9041";
+      var email = "Contact@unbelizeablewd.com"
+  }
+  getinTouch();
